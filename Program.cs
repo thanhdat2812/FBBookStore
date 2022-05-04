9aaef91a5f654dc52a3c6435250f22d2df6e6db1
DatThanh <84821265+thanhdat2812@users.noreply.github.com>
2022-05-04 11:08:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using fptbook.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("ConnStr");
+builder.Services.AddDbContext<FPTBookContext>(x => x.UseSqlServer(connectionString));
+//"DefaultConnection": "Server=.;Database=;Trusted_Connection=True;MultipleActiveResultSets=true"
+// Data Source=210.245.90.239;Initial Catalog=acomptec_1670;User ID=1670test;Password=v3qi43G#3
+//"ConnStrDemo": "Data Source=210.245.90.239;Initial Catalog=acomptec_1670;User ID=1670test;Password=v3qi43G#3"
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build(); 
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
